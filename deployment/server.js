1a3a0945cc83cc15e75c35ffcead682c6d34ac13
vangapandu-vasu <naiduvasu72@gmail.com>
2025-05-09 16:34:03 +0530

    ,

--- deployment/server.js
+++ deployment/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app=express();
 
+app.get("/",(req,res)=>{
+    res.send("deployed");
+})
+
 app.listen(dotenv.process.PORT() || 9000,()=>{
     console.log("server is running");
 });
